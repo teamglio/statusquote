149723afe9edae0a004a255cefc20ff17419d649
Emile Silvis <emile@silvis.co.za>
2013-03-28 12:20:32 +0200

    Mixup referral ID

--- quote.rb
+++ quote.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
-require 'rest_client'
+require 'rest-client'
 require 'json'
 
 get '/' do
 	@quote = RestClient.get 'http://www.iheartquotes.com/api/v1/random?source=oneliners'
-	@mixup_ad = RestClient.get 'http://serve.mixup.hapnic.com/9392697'
+	@mixup_ad = RestClient.get 'http://serve.mixup.hapnic.com/9766793'
 	erb :quote
 end
